6572c0db75179a3b7752483a40d388dd7fd565b1
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-06 22:28:42 +0000

    added base functions

--- HPPS/functions.hpp
+++ HPPS/functions.hpp
@@ -1,5 +1,18 @@
 #ifndef FUNCTIONS_HPP
 #define FUNCTIONS_HPP
 
+using namespace std;
+
+enum class Direcao{
+    Norte,
+    Sul,
+    Leste, 
+    Oeste,
+    Null
+};
+
+Direcao qualDir(pair<int, int> ori, pair<int, int> dest);
+
+int trocou(Direcao bef, Direcao now);
 
 #endif
--- src/functions.cpp
+++ src/functions.cpp
@@ -1,2 +1,19 @@
 #include <iostream>
 #include "../HPPS/functions.hpp"
+
+Direcao qualDir(pair<int, int> ori, pair<int, int> dest){
+    int x = dest.first - ori.first;
+    int y = dest.second - ori.second;
+    if(x == -1 && !y)   return Direcao::Norte;
+    if(x && !y)         return Direcao::Sul;
+    if(!x && y)         return Direcao::Leste;
+    if(!x && y == -1)   return Direcao::Oeste;
+    return Direcao::Null;
+}
+
+int trocou(Direcao bef, Direcao now){
+    if(bef == Direcao::Null || bef == now){
+        return 0;
+    }
+    return 3;
+}
