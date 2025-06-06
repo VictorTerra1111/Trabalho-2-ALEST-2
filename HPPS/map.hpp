d180448c9ee137e7b55609ed1f5f481c0045df1e
João Victor Terra Pereira <123105471+VictorTerra1111@users.noreply.github.com>
2025-06-06 18:32:15 +0000

    added map class

--- HPPS/map.hpp
+++ HPPS/map.hpp
@@ -1,5 +1,30 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include <vector>
+#include <string>
+#include <map>
+
+using namespace std;
+
+class Mapa
+{
+public:
+    Mapa(const std::string &caminhoArquivo);
+
+    const std::vector<std::string> &getGrade() const;
+    int getLinhas() const;
+    int getColunas() const;
+    std::map<char, std::pair<int, int>> getPortos() const;
+
+private:
+    std::vector<std::string> grade;
+    int linhas;
+    int colunas;
+    std::map<char, std::pair<int, int>> portos;
+
+    void carregar(const std::string &caminhoArquivo);
+    void achaPortos();
+};
 
 #endif
--- src/map.cpp
+++ src/map.cpp
@@ -1,2 +1,57 @@
 #include <iostream>
 #include "../HPPS/map.hpp"
+#include <fstream>
+#include <string> 
+
+Mapa::Mapa(const std::string &caminhoArquivo)
+{
+    carregar(caminhoArquivo);
+    achaPortos();
+}
+
+void Mapa::carregar(const std::string &caminhoArquivo)
+{
+    std::ifstream entrada(caminhoArquivo);
+    if (!entrada.is_open())
+    {
+        std::cerr << "ERRO: " << caminhoArquivo << std::endl;
+        return;
+    }
+
+    entrada >> linhas >> colunas;
+    entrada.ignore();
+
+    std::string linha;
+    while (std::getline(entrada, linha))
+    {
+        if (!linha.empty())
+        {
+            grade.push_back(linha);
+        }
+    }
+
+    entrada.close();
+}
+
+void Mapa::achaPortos()
+{
+    for (int i = 0; i < grade.size(); ++i)
+    {
+        for (int j = 0; j < grade[i].size(); ++j)
+        {
+            char c = grade[i][j];
+            if (c >= '1' && c <= '9')
+            {
+                portos[c] = {i, j};
+            }
+        }
+    }
+}
+
+const std::vector<std::string> &Mapa::getGrade() const { return grade; }
+
+int Mapa::getLinhas() const { return linhas; }
+
+int Mapa::getColunas() const { return colunas; }
+
+std::map<char, std::pair<int, int>> Mapa::getPortos() const { return portos; }
